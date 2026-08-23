2bbbfcba2009cdf370df33b24adac32651596bb5
Hooman MOD <hnmodeq@gmail.com>
2026-08-23 14:58:15 +0330

    editing the camera

--- components/canvas/scene/sceneConfig.ts
+++ components/canvas/scene/sceneConfig.ts
@@ -14,29 +14,29 @@ export const SCENE_CONFIG: SceneConfig = {
     {
       id: "stop_1_overview",
       progress: 0,
-      camera: [2.5, 1.5, -20.5],
-      target: [13.4, 1.2, -0.5],
+      camera: [15.036727, 1.036661, -12],
+      target: [15, 1.2, 1.5],
       fov: 45
     },
     {
       id: "stop_2_hyper",
       progress: 0.16,
-      camera: [19.5, 2.6, -2],
-      target: [14.9, 1.2, 1.2],
-      fov: 40
+      camera: [15.005536, 0.720672, -5.544793],
+      target: [15, 1, 1.5],
+      fov: 45
     },
     {
       id: "stop_3_hyper_reply",
       progress: 0.3,
-      camera: [28, 4.2, -3.2],
-      target: [15.1, 1.8, 1.5],
+      camera: [15.054576, 0.5, -1.830526],
+      target: [13, 3, 2.5],
       fov: 42
     },
     {
       id: "stop_4_building7",
       progress: 0.44,
-      camera: [9.6, 3.4, 6.4],
-      target: [14.2, 2.2, 2.9],
+      camera: [14.190916, 1, -1.66638],
+      target: [14.2, 1, 2.9],
       fov: 40
     },
     {
