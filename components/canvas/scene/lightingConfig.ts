fed60bd878a175ee1622619a6f839aabd75f2506
Hooman MOD <hnmodeq@gmail.com>
2026-08-23 10:13:27 +0330

    editing lights

--- components/canvas/scene/lightingConfig.ts
+++ components/canvas/scene/lightingConfig.ts
@@ -12,25 +12,25 @@ export const LIGHTS_CONFIG: LightConfig[] = [
     id: "blender_single_point_light",
     type: "point",
     color: 0xffffff,
-    intensity: 1500,
-    position: [13.5, 15, -4.5],
-    distance: 68,
-    decay: 2.1,
-    radius: 5.6,
+    intensity: 150,
+    position: [13.5, 8, -0.5],
+    distance: 18,
+    decay: 0.2,
+    radius: 6.7,
     castShadow: true,
-    enabled: false,
+    enabled: true,
     shadowMapSize: 512,
-    shadowBias: -0.0027
+    shadowBias: 0.0005
   },
   {
     id: "area_soft_key",
     type: "rectarea",
     color: 0xffffff,
-    intensity: 9.1,
+    intensity: 0,
     position: [13.5, 18, -0.5],
     target: [13.5, 0, -0.5],
     width: 68.1,
     height: 38.3,
-    enabled: true
+    enabled: false
   }
 ];
