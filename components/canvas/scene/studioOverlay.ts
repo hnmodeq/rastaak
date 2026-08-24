08ec33a1479825a9a1a0dd8e642f3907a9c6020d
Hooman MOD <hnmodeq@gmail.com>
2026-08-24 13:35:20 +0330

    editing shadows/colors

--- components/canvas/scene/studioOverlay.ts
+++ components/canvas/scene/studioOverlay.ts
@@ -17,5 +17,6 @@ export const STUDIO_OVERLAY: StudioOverlayConfig = {
   showTargetGizmo: false,
   showCamPath: false,
   showTargetPath: false,
-  showLightGizmos: false
+  showLightGizmos: false,
+  showHero: false
 };
