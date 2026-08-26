2d1b9bcc9593ce642d8267871aa07d4553219a59
Hooman MOD <hnmodeq@gmail.com>
2026-08-26 10:29:31 +0000

    Keep layout text inputs focused

--- components/layout-control/LayoutControlPanel.tsx
+++ components/layout-control/LayoutControlPanel.tsx
@@ -272,11 +272,11 @@ export function LayoutControlPanel() {
             </label>
             <label>
               <span>Title line 1</span>
-              <input value={SITE_CONTENT.features.titleLine1} onChange={(event) => changeText(() => { SITE_CONTENT.features.titleLine1 = event.target.value; })} />
+              <input defaultValue={SITE_CONTENT.features.titleLine1} onInput={(event) => changeText(() => { SITE_CONTENT.features.titleLine1 = event.currentTarget.value; })} />
             </label>
             <label>
               <span>Title line 2</span>
-              <input value={SITE_CONTENT.features.titleLine2} onChange={(event) => changeText(() => { SITE_CONTENT.features.titleLine2 = event.target.value; })} />
+              <input defaultValue={SITE_CONTENT.features.titleLine2} onInput={(event) => changeText(() => { SITE_CONTENT.features.titleLine2 = event.currentTarget.value; })} />
             </label>
             {SITE_CONTENT.features.items.map((item, index) => (
               <fieldset key={`feature-${index}`}>
@@ -298,11 +298,11 @@ export function LayoutControlPanel() {
                 {item.iconImage ? <button type="button" onClick={() => change(() => { item.iconImage = undefined; })}>Use built-in icon</button> : null}
                 <label>
                   <span>Item title</span>
-                  <input value={item.title} onChange={(event) => changeText(() => { item.title = event.target.value; })} />
+                  <input defaultValue={item.title} onInput={(event) => changeText(() => { item.title = event.currentTarget.value; })} />
                 </label>
                 <label>
                   <span>Description</span>
-                  <textarea rows={3} value={item.description} onChange={(event) => changeText(() => { item.description = event.target.value; })} />
+                  <textarea rows={3} defaultValue={item.description} onInput={(event) => changeText(() => { item.description = event.currentTarget.value; })} />
                 </label>
                 <button type="button" className="layout-danger" disabled={SITE_CONTENT.features.items.length <= 1} onClick={() => change(() => { SITE_CONTENT.features.items.splice(index, 1); })}>Remove item</button>
               </fieldset>
@@ -390,18 +390,18 @@ export function LayoutControlPanel() {
             </label>
             <label>
               <span>Section title</span>
-              <textarea rows={3} value={SITE_CONTENT.faq.title} onChange={(event) => changeText(() => { SITE_CONTENT.faq.title = event.target.value; })} />
+              <textarea rows={3} defaultValue={SITE_CONTENT.faq.title} onInput={(event) => changeText(() => { SITE_CONTENT.faq.title = event.currentTarget.value; })} />
             </label>
             {SITE_CONTENT.faq.items.map((item, index) => (
               <fieldset key={`faq-${index}`}>
                 <legend>Question {index + 1}</legend>
                 <label>
                   <span>Question</span>
-                  <textarea rows={2} value={item.question} onChange={(event) => changeText(() => { item.question = event.target.value; })} />
+                  <textarea rows={2} defaultValue={item.question} onInput={(event) => changeText(() => { item.question = event.currentTarget.value; })} />
                 </label>
                 <label>
                   <span>Answer</span>
-                  <textarea rows={4} value={item.answer} onChange={(event) => changeText(() => { item.answer = event.target.value; })} />
+                  <textarea rows={4} defaultValue={item.answer} onInput={(event) => changeText(() => { item.answer = event.currentTarget.value; })} />
                 </label>
                 <button type="button" className="layout-danger" disabled={SITE_CONTENT.faq.items.length <= 1} onClick={() => change(() => { SITE_CONTENT.faq.items.splice(index, 1); })}>Remove question</button>
               </fieldset>
